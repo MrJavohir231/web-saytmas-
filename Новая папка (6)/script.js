376d7c07565a44d82f94159c2638fb507cecb716
MrJavohir231 <kurbonovj322@gmail.com>
2025-09-26 17:05:54 +0500

    Add files via upload

---
+++ Новая папка (6)/script.js
@@ -0,0 +1,12 @@
+let ism = prompt ('ismingizni kirating')
+let Familiya = prompt  ('Familiya kiriting')
+let Yoshingizni = prompt  ('Yoshingizni kirating')
+let Yoqtirgan = prompt  ('Yoqtirgan fanizni kirating')
+let Yashash = prompt  ('Yashash manzilizni kirating')
+
+console.log(`Ismingiz: ${ism}
+    Familiyangiz: ${Familiya}
+    Yoshingiz: ${Yoshingizni}
+    Yoqtirgan faniz: ${Yoqtirgan}
+    Yashash manzilingiz ${Yashash} `);
+
